8ea4156a51f28da1c99bc1942ed048d9656f2b85
Francesco Guarnaccia <francescoguarnaccia@MacBook-Air-di-Francesco-3.local>
2025-12-03 12:41:10 +0100

    updates

--- dbsi_optimized/preprocessing/__init__.py
+++ dbsi_optimized/preprocessing/__init__.py
@@ -4,4 +4,4 @@ Data preprocessing utilities: loading, validation, normalization.
 
 from .loader import load_dwi_data, create_synthetic_data
 
-__all__ = ['load_dwi_data', 'create_synthetic_data']
+__all__ = ['load_dwi_data']
