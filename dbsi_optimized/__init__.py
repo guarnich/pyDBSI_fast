cf1260f12e4ae07eb87310588f8683f9b2e38858
Francesco Guarnaccia <francescoguarnaccia@MacBook-Air-di-Francesco-3.local>
2025-12-04 16:04:41 +0100

    updates

--- dbsi_optimized/__init__.py
+++ dbsi_optimized/__init__.py
@@ -3,7 +3,7 @@
 __version__ = "1.0.0"
 __author__ = "Francesco Guarnaccia"
 
-from .models.fast_dbsi import DBSI_FastModel, DBSIResult, DBSIVolumeResult
+from .models.fast_dbsi import DBSI_FastModel, DBSIVolumeResult
 from .core.snr_estimation import estimate_snr_robust
 from .preprocessing.loader import load_dwi_data
 # Aggiungi questi import:
@@ -12,11 +12,10 @@ from .visualization import plot_design_matrix
 
 __all__ = [
     'DBSI_FastModel',
-    'DBSIResult',
     'DBSIVolumeResult',
     'estimate_snr_robust',
     'load_dwi_data',
     'run_hyperparameter_optimization',
-    'plot_design_matrix', # Esportata
+    'plot_design_matrix', 
     '__version__',
 ]
